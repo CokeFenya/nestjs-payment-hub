33b43ae32d7da5ee952997a78964d256c072cd53
CokeFenya <danilov.danil800@mail.ru>
2026-02-01 18:25:10 +0500

    update provider

--- dist/modules/tbank/core/tbank-core.module.js
+++ dist/modules/tbank/core/tbank-core.module.js
@@ -9,7 +9,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TbankCoreModule = void 0;
 const common_1 = require("@nestjs/common");
 const payment_hub_options_interface_1 = require("../../../common/interfaces/payment-hub-options.interface");
-const payment_hub_context_module_1 = require("../../../common/payment-hub-context.module"); // ✅ ДОБАВЬ
 const tbank_constants_1 = require("./config/tbank.constants");
 const tbank_http_client_1 = require("./http/tbank.http-client");
 let TbankCoreModule = class TbankCoreModule {
@@ -17,12 +16,11 @@ let TbankCoreModule = class TbankCoreModule {
 exports.TbankCoreModule = TbankCoreModule;
 exports.TbankCoreModule = TbankCoreModule = __decorate([
     (0, common_1.Module)({
-        imports: [payment_hub_context_module_1.PaymentHubContextModule], // ✅ ВОТ ЭТО ГЛАВНОЕ
         providers: [
             {
                 provide: tbank_constants_1.TBANK_OPTIONS,
                 useFactory: (opts) => {
-                    if (!opts.tbank) {
+                    if (!(opts === null || opts === void 0 ? void 0 : opts.tbank)) {
                         throw new Error('Tbank options are missing. Provide { tbank: { terminalKey, password } } in PaymentHubModule.forRoot(...)');
                     }
                     return opts.tbank;
@@ -31,6 +29,6 @@ exports.TbankCoreModule = TbankCoreModule = __decorate([
             },
             tbank_http_client_1.TbankHttpClient
         ],
-        exports: [tbank_constants_1.TBANK_OPTIONS, tbank_http_client_1.TbankHttpClient] // ✅ лучше экспортить и options тоже
+        exports: [tbank_constants_1.TBANK_OPTIONS, tbank_http_client_1.TbankHttpClient]
     })
 ], TbankCoreModule);
--- src/modules/tbank/core/tbank-core.module.ts
+++ src/modules/tbank/core/tbank-core.module.ts
@@ -2,18 +2,16 @@ import { Module } from '@nestjs/common'
 import type { PaymentHubModuleOptions } from '../../../common/interfaces/payment-hub-options.interface'
 import { PaymentHubOptionsSymbol } from '../../../common/interfaces/payment-hub-options.interface'
 import type { TbankOptions } from '../../../common/interfaces/tbank/tbank-options.interface'
-import { PaymentHubContextModule } from '../../../common/payment-hub-context.module' // ✅ ДОБАВЬ
 
 import { TBANK_OPTIONS } from './config/tbank.constants'
 import { TbankHttpClient } from './http/tbank.http-client'
 
 @Module({
-	imports: [PaymentHubContextModule], // ✅ ВОТ ЭТО ГЛАВНОЕ
 	providers: [
 		{
 			provide: TBANK_OPTIONS,
 			useFactory: (opts: PaymentHubModuleOptions): TbankOptions => {
-				if (!opts.tbank) {
+				if (!opts?.tbank) {
 					throw new Error(
 						'Tbank options are missing. Provide { tbank: { terminalKey, password } } in PaymentHubModule.forRoot(...)'
 					)
@@ -24,6 +22,6 @@ import { TbankHttpClient } from './http/tbank.http-client'
 		},
 		TbankHttpClient
 	],
-	exports: [TBANK_OPTIONS, TbankHttpClient] // ✅ лучше экспортить и options тоже
+	exports: [TBANK_OPTIONS, TbankHttpClient]
 })
 export class TbankCoreModule {}
