33f31f294db566b143384693e5b4b14c5ce55918
CokeFenya <danilov.danil800@mail.ru>
2026-02-01 18:18:05 +0500

    update provider

--- dist/modules/tbank/core/tbank-core.module.js
+++ dist/modules/tbank/core/tbank-core.module.js
@@ -9,6 +9,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.TbankCoreModule = void 0;
 const common_1 = require("@nestjs/common");
 const payment_hub_options_interface_1 = require("../../../common/interfaces/payment-hub-options.interface");
+const payment_hub_context_module_1 = require("../../../common/payment-hub-context.module"); // ✅ ДОБАВЬ
 const tbank_constants_1 = require("./config/tbank.constants");
 const tbank_http_client_1 = require("./http/tbank.http-client");
 let TbankCoreModule = class TbankCoreModule {
@@ -16,6 +17,7 @@ let TbankCoreModule = class TbankCoreModule {
 exports.TbankCoreModule = TbankCoreModule;
 exports.TbankCoreModule = TbankCoreModule = __decorate([
     (0, common_1.Module)({
+        imports: [payment_hub_context_module_1.PaymentHubContextModule], // ✅ ВОТ ЭТО ГЛАВНОЕ
         providers: [
             {
                 provide: tbank_constants_1.TBANK_OPTIONS,
@@ -29,6 +31,6 @@ exports.TbankCoreModule = TbankCoreModule = __decorate([
             },
             tbank_http_client_1.TbankHttpClient
         ],
-        exports: [tbank_http_client_1.TbankHttpClient]
+        exports: [tbank_constants_1.TBANK_OPTIONS, tbank_http_client_1.TbankHttpClient] // ✅ лучше экспортить и options тоже
     })
 ], TbankCoreModule);
--- src/modules/tbank/core/tbank-core.module.ts
+++ src/modules/tbank/core/tbank-core.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import type { PaymentHubModuleOptions } from '../../../common/interfaces/payment-hub-options.interface'
 import { PaymentHubOptionsSymbol } from '../../../common/interfaces/payment-hub-options.interface'
 import type { TbankOptions } from '../../../common/interfaces/tbank/tbank-options.interface'
+import { PaymentHubContextModule } from '../../../common/payment-hub-context.module' // ✅ ДОБАВЬ
 
 import { TBANK_OPTIONS } from './config/tbank.constants'
 import { TbankHttpClient } from './http/tbank.http-client'
 
 @Module({
+	imports: [PaymentHubContextModule], // ✅ ВОТ ЭТО ГЛАВНОЕ
 	providers: [
 		{
 			provide: TBANK_OPTIONS,
@@ -22,6 +24,6 @@ import { TbankHttpClient } from './http/tbank.http-client'
 		},
 		TbankHttpClient
 	],
-	exports: [TbankHttpClient]
+	exports: [TBANK_OPTIONS, TbankHttpClient] // ✅ лучше экспортить и options тоже
 })
 export class TbankCoreModule {}
