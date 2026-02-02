5b134a37b5f6f0d335ac394fa7e999455f85eb0b
CokeFenya <danilov.danil800@mail.ru>
2026-02-02 14:48:18 +0500

    update provider

--- dist/modules/tbank/core/config/tbank.constants.js
+++ dist/modules/tbank/core/config/tbank.constants.js
@@ -2,5 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TBANK_API_VERSION = exports.TBANK_API_BASE_URL_TEST = exports.TBANK_API_BASE_URL_PROD = void 0;
 exports.TBANK_API_BASE_URL_PROD = 'https://securepay.tinkoff.ru';
-exports.TBANK_API_BASE_URL_TEST = 'https://rest-api-test.tinkoff.ru';
+exports.TBANK_API_BASE_URL_TEST = 'https://securepay.tinkoff.ru';
 exports.TBANK_API_VERSION = 'v2';
